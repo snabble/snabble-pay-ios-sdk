c16e17e3aba2404d36c8fc4836e5bd4165eaa819
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2022-12-08 10:14:27 +0100

    add example app

---
+++ Example/SnabblePay/SnabblePayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnabblePayApp.swift
+//  SnabblePay
+//
+//  Created by Andreas Osberghaus on 2022-12-08.
+//
+
+import SwiftUI
+
+@main
+struct SnabblePayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
