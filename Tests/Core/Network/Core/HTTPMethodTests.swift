711c526a53bf8fc255a04b6a765054d7832b154c
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2022-12-13 08:25:55 +0100

    test httpmethods value

--- Tests/Core/Network/Core/HTTPMethodTests.swift
+++ Tests/Core/Network/Core/HTTPMethodTests.swift
@@ -5,4 +5,37 @@
 //  Created by Andreas Osberghaus on 2022-12-12.
 //
 
-import Foundation
+import XCTest
+@testable import SnabblePayCore
+
+final class HTTPMethodTests: XCTestCase {
+    func testGETValue() throws {
+        let method: HTTPMethod = .get(nil)
+        XCTAssertEqual(method.value, "GET")
+    }
+
+    func testPATCHValue() throws {
+        let method: HTTPMethod = .patch(nil)
+        XCTAssertEqual(method.value, "PATCH")
+    }
+
+    func testPUTValue() throws {
+        let method: HTTPMethod = .put(nil)
+        XCTAssertEqual(method.value, "PUT")
+    }
+
+    func testPOSTValue() throws {
+        let method: HTTPMethod = .post(nil)
+        XCTAssertEqual(method.value, "POST")
+    }
+
+    func testDELETEValue() throws {
+        let method: HTTPMethod = .delete
+        XCTAssertEqual(method.value, "DELETE")
+    }
+
+    func testHEADValue() throws {
+        let method: HTTPMethod = .head
+        XCTAssertEqual(method.value, "HEAD")
+    }
+}
