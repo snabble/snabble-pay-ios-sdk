f88ef93a440cc6d31896979ea9e4d40d3c52f41c
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2022-12-13 11:45:28 +0100

    add error tests

--- Tests/Core/Network/Core/URLSessionEndpointTests.swift
+++ Tests/Core/Network/Core/URLSessionEndpointTests.swift
@@ -13,6 +13,8 @@ final class URLSessionEndpointTests: XCTestCase {
 
     var cancellables = Set<AnyCancellable>()
 
+    // MARK: - Decodable
+
     func testDecodableCombine() async throws {
         let data = try loadResource(filename: "credentials", withExtension: "json")
         MockURLProtocol.error = nil
@@ -47,6 +49,33 @@ final class URLSessionEndpointTests: XCTestCase {
         wait(for: [expectation], timeout: 5.0)
     }
 
+    func testDecodableCombineError() async throws {
+        MockURLProtocol.error = URLError(.unknown)
+        MockURLProtocol.requestHandler = nil
+
+        let endpoint: Endpoint<Credentials> = .credentials()
+        let session = URLSession.mockSession
+
+        let expectation = expectation(description: "credentials")
+        session.publisher(for: endpoint)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    XCTAssertTrue(true)
+                case .failure(let error):
+                    XCTAssertNotNil(error)
+                    XCTAssertNotNil(error as? URLError)
+                    XCTAssertEqual((error as! URLError).code, .unknown)
+                }
+                expectation.fulfill()
+            } receiveValue: { credentials in
+                XCTAssertNil(credentials)
+            }
+            .store(in: &cancellables)
+
+        wait(for: [expectation], timeout: 5.0)
+    }
+
     func testDecodableAsync() async throws {
         let data = try loadResource(filename: "credentials", withExtension: "json")
         MockURLProtocol.error = nil
@@ -66,6 +95,23 @@ final class URLSessionEndpointTests: XCTestCase {
         let decodableObject = try await session.object(for: endpoint)
 
         XCTAssertNotNil(decodableObject)
+    }
+
+    func testDecodableAsyncError() async throws {
+        MockURLProtocol.error = URLError(.unknown)
+        MockURLProtocol.requestHandler = nil
+
+        let endpoint: Endpoint<Credentials> = .credentials()
+        let session = URLSession.mockSession
+
+        do {
+            let decodableObject = try await session.object(for: endpoint)
+            XCTAssertNil(decodableObject)
+        } catch {
+            XCTAssertNotNil(error)
+            XCTAssertNotNil(error as? URLError)
+            XCTAssertEqual((error as! URLError).code, .unknown)
+        }
     }
 
     func testDecodable() async throws {
@@ -91,6 +137,31 @@ final class URLSessionEndpointTests: XCTestCase {
                 XCTAssertNil(error)
             case .success(let credentials):
                 XCTAssertNotNil(credentials)
+            }
+            expectation.fulfill()
+        }
+
+        dataTask.resume()
+
+        wait(for: [expectation], timeout: 5.0)
+    }
+
+    func testDecodableError() async throws {
+        MockURLProtocol.error = URLError(.unknown)
+        MockURLProtocol.requestHandler = nil
+
+        let endpoint: Endpoint<Credentials> = .credentials()
+        let session = URLSession.mockSession
+
+        let expectation = expectation(description: "credentials")
+        let dataTask = session.dataTask(for: endpoint) { result in
+            switch result {
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                XCTAssertNotNil(error as? URLError)
+                XCTAssertEqual((error as! URLError).code, .unknown)
+            case .success(let credentials):
+                XCTAssertNil(credentials)
             }
             expectation.fulfill()
         }
