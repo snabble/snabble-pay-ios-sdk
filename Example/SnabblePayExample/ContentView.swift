06eeb604c0738d182455660693ecbadac33bf738
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2023-01-13 15:22:01 +0100

    rename payment validation to account

--- Example/SnabblePayExample/ContentView.swift
+++ Example/SnabblePayExample/ContentView.swift
@@ -6,18 +6,42 @@
 //
 
 import SwiftUI
+import Combine
 import SnabblePayCore
 import SnabblePayNetwork
 
+class ViewModel: ObservableObject {
+    let networkManager: NetworkManager = .init(config: .init(customUrlScheme: "snabble-pay", apiKey: "IO2wX69CsqZUQ3HshOnRkO4y5Gy/kRar6Fnvkp94piA2ivUun7TC7MjukrgUKlu7g8W8/enVsPDT7Kvq28ycw=="))
+
+    private var cancellables = Set<AnyCancellable>()
+
+    func accountValidation() {
+        let endpoint = Endpoints.account(onEnvironment: .development)
+        networkManager.publisher(for: endpoint)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    print("finished")
+                case .failure(let error):
+                    print("error: ", error.localizedDescription)
+                }
+            } receiveValue: { validation in
+                print("validation: ", validation)
+            }
+            .store(in: &self.cancellables)
+    }
+}
+
 struct ContentView: View {
-    var networkManager: NetworkManager
+    @ObservedObject var viewModel: ViewModel
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello World!")
+            Button {
+                viewModel.accountValidation()
+            } label: {
+                Text("Payment Validation")
+            }
         }
         .padding()
     }
@@ -25,6 +49,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(networkManager: .init(config: .init(customUrlScheme: "snabble-pay", apiKey: "12345")))
+        ContentView(viewModel: .init())
     }
 }
