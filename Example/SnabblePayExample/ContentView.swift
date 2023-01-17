66c07d21a4df133d65212dd81da184eecf9c0d13
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2023-01-17 10:28:49 +0100

    improve readability

--- Example/SnabblePayExample/ContentView.swift
+++ Example/SnabblePayExample/ContentView.swift
@@ -12,12 +12,17 @@ import SnabblePayNetwork
 import BetterSafariView
 
 class ViewModel: ObservableObject {
-    let networkManager: NetworkManager = .init(config: .init(customUrlScheme: "snabble-pay", apiKey: "IO2wX69CsqZUQ3HshOnRkO4y5Gy/kRar6Fnvkp94piA2ivUun7TC7MjukrgUKlu7g8W8/enVsPDT7Kvq28ycw=="))
-
-    private var cancellables = Set<AnyCancellable>()
+    let networkManager: NetworkManager = .init(
+        config: .init(
+            customUrlScheme: "snabble-pay",
+            apiKey: "IO2wX69CsqZUQ3HshOnRkO4y5Gy/kRar6Fnvkp94piA2ivUun7TC7MjukrgUKlu7g8W8/enVsPDT7Kvq28ycw=="
+        )
+    )
 
     @Published var validationURL: URL?
     @Published var errorOccured: Bool = false
+
+    private var cancellables = Set<AnyCancellable>()
 
     func accountValidation() {
         let endpoint = Endpoints.account(onEnvironment: .development)
