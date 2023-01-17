f8e334866eb55ee3ec34f422c76b86b108e022fe
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2023-01-17 10:35:53 +0100

    fixes lint warnings

--- Example/SnabblePayExample/AppDelegate.swift
+++ Example/SnabblePayExample/AppDelegate.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         return true
     }
 }
