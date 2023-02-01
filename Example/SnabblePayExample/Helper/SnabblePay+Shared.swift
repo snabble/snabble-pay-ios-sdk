56501a3140e8498937b164741462d02e7b577f16
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2023-02-01 08:33:23 +0100

    set environment to development

--- Example/SnabblePayExample/Helper/SnabblePay+Shared.swift
+++ Example/SnabblePayExample/Helper/SnabblePay+Shared.swift
@@ -9,8 +9,12 @@ import Foundation
 import SnabblePayCore
 
 extension SnabblePay {
-    static let shared: SnabblePay = .init(
-        apiKey: "IO2wX69CsqZUQ3HshOnRkO4y5Gy/kRar6Fnvkp94piA2ivUun7TC7MjukrgUKlu7g8W8/enVsPDT7Kvq28ycw==",
-        session: .shared
-    )
+    static var shared: SnabblePay = {
+        let snabblePay: SnabblePay = .init(
+            apiKey: "IO2wX69CsqZUQ3HshOnRkO4y5Gy/kRar6Fnvkp94piA2ivUun7TC7MjukrgUKlu7g8W8/enVsPDT7Kvq28ycw==",
+            session: .shared
+        )
+        snabblePay.environment = .development
+        return snabblePay
+    }()
 }
