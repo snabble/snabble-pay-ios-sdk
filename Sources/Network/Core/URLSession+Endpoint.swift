a0afd45e49aff28544f699f75fe6304090cb594d
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2023-02-24 09:52:42 +0100

    move httperror in single file

--- Sources/Network/Core/URLSession+Endpoint.swift
+++ Sources/Network/Core/URLSession+Endpoint.swift
@@ -8,22 +8,6 @@
 import Foundation
 import Combine
 
-public enum HTTPError: Equatable {
-    case invalidResponse(HTTPStatusCode, Endpoints.Error?)
-    case unknownResponse(URLResponse)
-}
-
-extension HTTPError: LocalizedError {
-    public var errorDescription: String? {
-        switch self {
-        case let .invalidResponse(statusCode, error):
-            return "Error: \(statusCode) \(String(describing: error))"
-        case let .unknownResponse(response):
-            return "Error: unknown \(response)"
-        }
-    }
-}
-
 private extension URLResponse {
     func verify(with data: Data) throws {
         guard let httpResponse = self as? HTTPURLResponse else {
