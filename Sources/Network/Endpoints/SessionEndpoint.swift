2146799631e8f2d272500718c7fd093ec2b1a639
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2023-02-24 11:36:21 +0100

    fixes imports

--- Sources/Network/Endpoints/SessionEndpoint.swift
+++ Sources/Network/Endpoints/SessionEndpoint.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import Tagged
 
 public typealias ModelSession = Session
 
--- Sources/Network/Endpoints/TokenEndpoint.swift
+++ Sources/Network/Endpoints/TokenEndpoint.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import Tagged
 
 extension Endpoints {
     enum Token {
