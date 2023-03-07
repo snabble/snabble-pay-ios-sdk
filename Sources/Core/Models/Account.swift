4f38f13f393d02cfc6d98c6278a8ff50301fc765
Andreas Osberghaus <andreas.osberghaus@icloud.com>
2023-03-07 12:05:28 +0100

    add account documenation

--- Sources/Core/Models/Account.swift
+++ Sources/Core/Models/Account.swift
@@ -9,18 +9,39 @@ import Foundation
 import Tagged
 import SnabblePayNetwork
 
+/// The back account information
 public struct Account: Identifiable {
+    /// Unique identifier 
     public let id: ID
+
+    /// Name of the bank account
     public let name: String
+
+    /// Holder name of the bank account
     public let holderName: String
+
+    /// The currencyCode which is used by the bank account
     public let currencyCode: CurrencyCode
+
+    /// Name of the bank
     public let bank: String
-    public let createdAt: Date
+
+    /// Masked IBAN of the bank account
     public let iban: IBAN
+
+    /// State of the mandate linked to the account
     public let mandateState: Mandate.State
 
+    // Creation date of the bank account in our database
+    public let createdAt: Date
+
+    // Type Safe ID
     public typealias ID = Tagged<Account, String>
+
+    // Type Safe IBAN
     public typealias IBAN = Tagged<(Account, iban: ()), String>
+
+    // Type Safe CurrencyCode
     public typealias CurrencyCode = Tagged<(Account, currencyCode: ()), String>
 }
 
